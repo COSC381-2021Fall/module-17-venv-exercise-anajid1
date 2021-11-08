b8d6660ab5f8f7013fa8d8a594e0efa80ed6f31c
Abdullah Najid <anajid@emich.edu>
2021-11-08 02:39:47 +0000

    Added numpy package and example.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pyjokes
 import emoji
+import numpy as np
 
 print(pyjokes.get_joke())
 print(emoji.emojize(':grinning_face_with_smiling_eyes:'))
+
+a = np.arange(6)
+a2 = a[np.newaxis, :]
+print(a2.shape)
